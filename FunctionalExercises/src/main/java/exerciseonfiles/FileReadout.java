9ee13dec9ee89db7ca763d0ca274886bb0d83116
Develop5 <lourdesalternativa@yahoo.com>
2022-11-06 02:02:19 +0100

    Imperative searching word in file

--- FunctionalExercises/src/main/java/exerciseonfiles/FileReadout.java
+++ FunctionalExercises/src/main/java/exerciseonfiles/FileReadout.java
@@ -1,4 +1,38 @@
 package exerciseonfiles;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+
+
 public class FileReadout {
+    public static void main(String[] args) {
+        try {
+            String searchWord = " elit";
+            URL resource = FileReadout.class.getClassLoader().getResource("file_to_find_words.txt");
+            long count = 0;
+            BufferedReader bufferedReader =
+                    new BufferedReader(new FileReader(resource.getFile()));
+
+            String line = null;
+
+            while((line = bufferedReader.readLine()) != null) {
+                if(line.contains(searchWord)) {
+                    count++;
+                }
+            }
+
+            System.out.println("------->>>>> Times found: " + count + "\n");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
+
+
+
